245b29d2bb3223bdbb5462f0ab81c77603438c0d
Nilesh Baban Binnar <102431318+nilu0611@users.noreply.github.com>
2024-05-09 17:15:47 +0530

    Add files via upload

---
+++ dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "Nilesh@123";
+$dbName = "loginsystem";
+
+$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
---
+++ dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "Nilesh@123";
+$dbName = "loginsystem";
+
+$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
